e42eaa042a6f0d8bcb82cd3fdfcbb38851e5db62
Mac-anthony Arinze <moviesmpire@gmail.com>
2025-05-29 02:23:13 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -110,7 +110,7 @@ app.post('/api/checkout', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', (req, res) => {
+  app.get('http://localhost:5000', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
