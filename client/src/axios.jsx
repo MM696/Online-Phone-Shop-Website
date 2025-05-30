cacee86f9a3782a7d1ae4edc22f6f0c31d787785
MM696 <moviesmpire@gmail.com>
2025-05-30 02:13:47 +0100

    initial commit

--- client/src/axios.jsx
+++ client/src/axios.jsx
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
-const API_BASE =
-  import.meta.env.PROD
-    ? '/api' // relative path for production (same domain)
-    : 'http://localhost:5000/api'; // full URL for local dev
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 export default axios.create({
   baseURL: API_BASE,
-  withCredentials: true,
+  withCredentials: true, // optional
 });
